4c1d2b76f889f588b88944920b1d4f679f9ce552
Grzegorz Rozpędzik <rozpedzik@gmail.com>
2024-03-22 10:15:13 +0100

    vite_config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-deploy-demo/',
   server: {
     host: true,
     port: 5173, // This is the port which we will use in docker
